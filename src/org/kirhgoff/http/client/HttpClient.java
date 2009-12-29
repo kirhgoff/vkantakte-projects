8e6f0b13f2550e5f9af98542a4888391cb27bff3
kirill.lastovirya <kirill.lastovirya@1b4263b0-eff0-11de-9456-cf00f3bddd4e>
2009-12-29 22:16:57 +0000

    added real working stuff

--- src/org/kirhgoff/http/client/HttpClient.java
+++ src/org/kirhgoff/http/client/HttpClient.java
@@ -5,19 +5,12 @@ import org.kirhgoff.http.client.model.FilesList;
 public class HttpClient {
 
 	public static void main(String[] args) throws Exception {
-//		ServiceRunner runner = new ServiceRunner();
-//		runner.init();
-//
-//		try {
 			runTest();
-//		} finally {
-//			runner.stop();
-//		}
 	}
 
 	private static void runTest() throws Exception {
 		Command command = new Command("http://localhost:8080/MethodInvoker", Command.GET_LIST_OF_FILES);
-		command.addParameter("user_id", "kirill.lastovirya");
+		command.addParameter(Command.USER_ID, "kirill.lastovirya");
 
 		System.out.println("Printing file names:");
 		FilesList filesList = (FilesList) command.invoke();
